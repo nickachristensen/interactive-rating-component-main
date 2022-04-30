c59a18e0c05b32a8932eb47a01822972b6f415f3
Nicholas A Christensen <nickachristensen@gmail.com>
2022-04-30 15:43:26 -0400

    added some css and js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+ratingDiv = document.querySelector('.rating');
+
 const numbers = [1, 2, 3, 4, 5];
-const rating = numbers.map(number => {
+
+const rating = numbers.forEach(number => {
     const n = document.createElement("p");
     n.innerHTML = `${number}`;
-    return n;
+    ratingDiv.append(n);
 });
 
-div.append(rating);
+
