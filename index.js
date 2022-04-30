cecd0fa1416fd8e01411355e02e9a0150928462b
Nicholas A Christensen <nickachristensen@gmail.com>
2022-04-30 18:09:22 -0400

    javascript for rating

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ ratingDiv = document.querySelector('.rating');
 
 const numbers = [1, 2, 3, 4, 5];
 
+function handleClick(e) {
+  const clearRatings = document.querySelectorAll('.selected');  
+  clearRatings.className = 'number';
+  const target = e.target;
+  const rating = target.textContent;
+  target.className = "selected"
+}
+
 const rating = numbers.forEach(number => {
     const n = document.createElement("p");
+    n.className = "number"
     n.innerHTML = `${number}`;
+    n.addEventListener("click", handleClick)
     ratingDiv.append(n);
 });
 
