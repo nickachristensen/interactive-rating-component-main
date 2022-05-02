336af3107b95e6543ee5f9675fc3e32d30c6c338
Nicholas A Christensen <nickachristensen@gmail.com>
2022-05-01 22:29:51 -0400

    submit js and css added for rating submit

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const ratings = document.querySelectorAll('.number');
 ratings.forEach(e => {e.addEventListener("click", handleClick)});
 
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", handleSubmit);
+
+function handleSubmit() {
+    main = document.querySelector(".main");
+    main.style.display = "none";
+    thankYou = document.querySelector(".thank-you");
+    thankYou.style.visibility = "visible";
+    thankYou.textContent =
+    `You selected ${finalRating} out of 5
+
+  Thank you!
+
+  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch!`;
+
+}
+
+
 function handleClick(e) {
     if (e.target.classList.contains('selected')) {
         e.target.classList.remove('selected');
@@ -11,6 +30,7 @@ function handleClick(e) {
         console.log(finalRating)
     }
 }
+
 
 
 
